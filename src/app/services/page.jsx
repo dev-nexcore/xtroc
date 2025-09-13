175248f14138a7f9cafe291fc511488537db6b44
AbdulMaaz909 <abdulmaaz827@gmail.com>
2025-09-13 14:51:09 +0530

    changes implement in navbar on service tab add name category and images like product tab!

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,29 +1,3 @@
-// import ChatBoat from '@/components/ChatBoat/ChatBoat';
-// import OurServices from '@/components/Services/OurServices';
-// import Services from '@/components/Services/Services';
-// import WhatsappLogo from '@/components/WhatsappLogo/WhatsappLogo';
-// import Footer from '@/layout/footer';
-// import MobileNavbar from '@/layout/mobileNavbar';
-// import Navbar from '@/layout/Navbar';
-// import React from 'react'
-
-// const page = () => {
-//   return (
-//     <div>
-//       <Navbar/>
-//       <MobileNavbar/>
-//         <Services/>
-//         <OurServices/>
-//         <WhatsappLogo/>
-//         <ChatBoat/>
-//         <Footer/>
-//     </div>
-//   )
-// }
-
-// export default page;
-
-
 import ChatBoat from "@/components/ChatBoat/ChatBoat";
 import Services from "@/components/Services/Services";
 import OurServices from "@/components/Services/OurServiceMobile";
